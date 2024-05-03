8cda7e845fb581e0ebcdff09e2d6b83bc92cf57b
DanielHdzHdz <daniel.dhpumas@comunidad.unam.mx>
2024-05-03 01:06:44 -0600

    actividad terminada

---
+++ Unidad5_Actividad_Complementaria2.c
@@ -0,0 +1,24 @@
+//Universidas Atonoma Nacional de Mexico
+//FACULTAD DE CONTADURÍA Y ADMINISTRACIÓN
+//INFORMÁTICA A DISTANCIA
+// Programa creado por Daniel Hernandez Hernandez el  03/05/2024.
+//Asignatura: 2125-Diseño de algoritmos
+//Docente: Juan Manuel Martínez Fernández
+#include <stdio.h>
+
+int main(void) {
+    int n, a;
+
+    printf("Ingrese un numero:\n");
+    scanf("%d", &n);
+
+    for(a = 1; a <= n; a++) {
+        int resultado = a * (a + 1) * (2 * a + 1) / 6;
+        printf("%d, ", resultado);
+    }
+
+    return 0;
+    // Programador: Daniel Hernandez Hernandez
+    // Fecha: 03/05/2024
+    // Programa para calcular la suma de los cuadrados de los primeros n numeros naturales
+}
